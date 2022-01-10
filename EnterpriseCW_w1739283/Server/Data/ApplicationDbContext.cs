4f16beb6a8b99cf6e594af86cd902261d0e14a11
sifu <gulle.firenze@hotmail.com>
2022-01-10 23:56:48 +0000

    Add project files.

---
+++ EnterpriseCW_w1739283/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using EnterpriseCW_w1739283.Server.Models;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EnterpriseCW_w1739283.Shared;
+
+namespace EnterpriseCW_w1739283.Server.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        // public DbSet<WeatherForecast> WeatherForecasts { get; set; }
+        public ApplicationDbContext(
+            DbContextOptions<ApplicationDbContext> options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+        public DbSet<EnterpriseCW_w1739283.Shared.Todo> todo { get; set; }
+        public DbSet<EnterpriseCW_w1739283.Shared.Company> Company { get; set; }
+        public DbSet<EnterpriseCW_w1739283.Shared.Project> Project { get; set; }
+        public DbSet<EnterpriseCW_w1739283.Shared.User> User { get; set; }
+
+    }
+}
---
+++ EnterpriseCW_w1739283/Shared/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnterpriseCW_w1739283.Shared
+{
+    public class User
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string UserName { get; set; }
+    }
+}
